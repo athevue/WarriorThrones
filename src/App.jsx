9f530b6299b591c5056a57c3eeb37647ca0e2e46
Adam Vue <athevue@gmail.com>
2025-09-15 00:43:57 -0400

    Updated app.jsx to revoke testing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-// import Home from "./pages/home";
-
-// function App() {
-//   return (
-//     <div className="flex flex-col min-h-screen bg-gray-50">
-//       <Home />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import TestSupabase from "./components/test";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div>
-      <h1>Supabase Test</h1>
-      <TestSupabase />
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Home />
     </div>
   );
 }
