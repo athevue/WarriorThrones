25909a64ae2400c3307d2370318ec9fd48b0e381
Adam Vue <athevue@gmail.com>
2025-09-22 18:45:50 -0400

    added quick actions section (unfinished)

--- src/components/quickActions.jsx
+++ src/components/quickActions.jsx
@@ -1,2 +1,24 @@
-import "../quickActions.css";
+import "./quickActions.css";
 
+export default function Header() {
+    return (
+        <div>
+            <h1>Quick Actions Section</h1>
+            <div className="buttons">
+                <button className="quick-button">
+                    <span className="button-icon">+</span> Add Bathroom
+                </button>
+                <button className="quick-button">
+                    <span className="button-icon"></span> Browse Bathrooms
+                </button>
+                <button className="quick-button">
+                    <span className="button-icon"></span> Top Rated Bathrooms
+                </button>
+                <button className="quick-button">
+                    <span className="button-icon"></span> Report Bathroom
+                </button>
+            </div>
+            
+        </div>
+    );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,11 @@
 import Header from "../components/header";
+import Quick from "../components/quickActions";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
-      <main className="flex-1 max-w-4xl mx-auto p-6">
-        <p className="text-black">Welcome! Here will be the rest of the components.</p>
-      </main>
+      <Quick />
     </div>
   );
 }
