db0d711195f737cc7bbc08da67520be02fd6a918
Adam Vue <athevue@gmail.com>
2025-09-22 18:25:28 -0400

    putting project in normal state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// import Home from "./pages/home";
-
-// function App() {
-//   return (
-//     <div className="flex flex-col min-h-screen bg-gray-50">
-//       <Home />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import TestSupabase from "./components/test";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div>
-      <h1>Supabase Test</h1>
-      <TestSupabase />
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+// import TestSupabase from "./components/test";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Supabase Test</h1>
+//       <TestSupabase />
+//     </div>
+//   );
+// }
+
+// export default App;
